5854beec2e7c4c3ba203c551fdf77f29c3550300
ShimengDai <daishimeng@gmail.com>
2024-09-14 21:40:49 -0400

    1

--- 1_prepare_data.py
+++ 1_prepare_data.py
@@ -1,114 +1,101 @@
 import os, sys
 import yaml
-import re, string
-import random
 import pickle
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, train_test_split
 from utils.preprocessing import preprocess_text, remove_stopwords, get_wordnet_pos, lemmatizer, preprocessing_compose
 from utils.feature_extraction import *
 from utils.dataset import clean_data, balance_data, print_data_dims, TextDataset
-from scipy.linalg import get_blas_funcs
-
-
 
 def prepare_data(args):
     """
-    Prepares the datasets for the experiment.
+    Prepares the datasets for cross-validation with a separate hold-out test set.
     """
-    # Get configurations
+    # Load configurations
     config = yaml.load(open(args.conf_dir, 'r'), Loader=yaml.FullLoader)
     data_path = os.path.join(config['Corpus']['data_dir'], config['Corpus']['data_filename'])
     n_classes = config['Data_setup']['n_classes']
     text_col = config['Data_setup']['text_col']
     label_col = config['Data_setup']['label_col']
     labels_to_ignore = config['Data_setup']['labels_to_ignore']
-    random_seed = config['Data_setup']['random_seed']
-    val_ratio = config['Data_setup']['val_ratio']
+    random_seed = config['cross_validation_setup']['random_seed']
+    k_folds = config['cross_validation_setup']['k_folds']  # k-folds for cross-validation
     test_ratio = config['Data_setup']['test_ratio']
     feat_extraction_method = config['Feature_extraction']['method']
     feat_extraction_configs = config['Feature_extraction'][feat_extraction_method + '_configs']
 
-    assert (val_ratio + test_ratio) < 1.0, 'Sum of val_ratio and test_ratio should be lower than 1.0.'
-
-    # Get data and clean data
+    # Get and clean data
     data = pd.read_csv(data_path)
     data = clean_data(data, label_col, labels_to_ignore, n_classes)
     print(f'Found {len(data.index)} valid tweets.')
 
-    # Balance data if needed
+    # Balance data if required
     if config['Data_setup']['balance_data']:
         print('Balancing the data so all classes have equal no. of observations (value: count)')
         data = balance_data(data, label_col, random_seed)
 
-    # Conditional text preprocessing based on feature extraction method
-    if feat_extraction_method in ['BERTEmbedding', 'GPT2Embedding']:
-        print('Using raw text for BERT or GPT-2...')
-        text_data = data[text_col].fillna('').astype(str)
-    else:  # TFIDF or Word2Vec
-        print('Preprocessing tweets...', end=' ')
-        data = data.dropna(subset=[text_col])
-        data[text_col] = data[text_col].fillna('').astype(str)
-        data['clean_text'] = data[text_col].apply(lambda x: preprocessing_compose(x))
-        print('Done')
-        text_data = data['clean_text']
+    X = data[text_col].values
+    y = data[label_col].values
 
-    # Save a copy of the clean data as CSV
-    data_cleaned_fn = os.path.basename(data_path).split('.')[0] + '_cleaned.csv'
-    data.to_csv('data/' + data_cleaned_fn, index=False)
+    # Split data into training-validation and test sets
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=test_ratio, random_state=random_seed)
+    print(f'Train/Validation size: {len(X_train_val)}, Test size: {len(X_test)}')
 
-    # Data split
-    print(f'Splitting data into training, validation, and test sets and extracting features (method: {feat_extraction_method})...', end=' ')
-    
-    random.seed(random_seed)
-    X_train, X_test, y_train, y_test = train_test_split(text_data, data[label_col],
-                                                        test_size=val_ratio + test_ratio, 
-                                                        random_state=random_seed, shuffle=True)
-    X_val, X_test, y_val, y_test = train_test_split(X_test, y_test,
-                                                    test_size=test_ratio / (val_ratio + test_ratio),
-                                                    random_state=random_seed,
-                                                    shuffle=False)
+    # Initialize k-fold cross-validation for the training-validation set
+    kf = KFold(n_splits=k_folds, shuffle=True, random_state=random_seed)
 
-    # Select feature extraction method and apply
-    feature_extractor = getattr(sys.modules[__name__], feat_extraction_method)(feat_extraction_configs)
+    fold_idx = 0
+    for train_index, val_index in kf.split(X_train_val):
+        print(f'Preparing fold {fold_idx + 1}/{k_folds}...')
+        X_train, X_val = X_train_val[train_index], X_train_val[val_index]
+        y_train, y_val = y_train_val[train_index], y_train_val[val_index]
 
-    X_train_vectors, X_val_vectors, X_test_vectors = feature_extractor(data=data,
-                                                                       X_train=X_train,
-                                                                       X_val=X_val,
-                                                                       X_test=X_test)
-    print('Done')
+        # Feature extraction based on the specified method
+        feature_extractor = getattr(sys.modules[__name__], feat_extraction_method)(feat_extraction_configs)
 
-    # Save the fitted feature extractor
-    feat_extractor_path = os.path.join(args.exp_dir, 'trained_models', feat_extraction_method + '_fitted.pkl')
-    with open(feat_extractor_path, 'wb') as fe:
-        pickle.dump(feature_extractor, fe)
+        X_train_vectors, X_val_vectors = feature_extractor(data=data,
+                                                           X_train=X_train,
+                                                           X_val=X_val,
+                                                           X_test=None)  # No test set during cross-validation
+        print(f'Fold {fold_idx + 1} - Feature extraction complete.')
 
-    # Print out dataset dimensions
-    print_data_dims(X_train_vectors, X_val_vectors, X_test_vectors, y_train, y_val, y_test)
+        # Save the fitted feature extractor for this fold
+        feat_extractor_path = os.path.join(args.exp_dir, 'trained_models', f'{feat_extraction_method}_fold{fold_idx + 1}_fitted.pkl')
+        with open(feat_extractor_path, 'wb') as fe:
+            pickle.dump(feature_extractor, fe)
 
-    # Export the datasets
-    datasets_path = 'exp/datasets'
-    if not os.path.exists(datasets_path):
-        os.makedirs(datasets_path)
+        # Export the datasets for this fold
+        datasets_path = os.path.join(args.exp_dir, 'datasets', f'fold_{fold_idx + 1}')
+        if not os.path.exists(datasets_path):
+            os.makedirs(datasets_path)
 
-    print(f'Exporting the datasets to {datasets_path}...', end=' ')
-    train_dataset = TextDataset(X_train_vectors, y_train)
-    valid_dataset = TextDataset(X_val_vectors, y_val)
+        print(f'Exporting fold {fold_idx + 1} datasets to {datasets_path}...')
+        train_dataset = TextDataset(X_train_vectors, y_train)
+        valid_dataset = TextDataset(X_val_vectors, y_val)
+
+        with open(os.path.join(datasets_path, 'train_data.pkl'), 'wb') as tr:
+            pickle.dump(train_dataset, tr)
+        with open(os.path.join(datasets_path, 'valid_data.pkl'), 'wb') as va:
+            pickle.dump(valid_dataset, va)
+
+        fold_idx += 1
+
+    # Feature extraction for the test set
+    print("Feature extraction for the test set...")
+    X_test_vectors = feature_extractor(data=data, X_train=None, X_val=None, X_test=X_test)[2]
+
+    # Save the test set
     test_dataset = TextDataset(X_test_vectors, y_test)
-
-    with open(os.path.join(datasets_path, 'train_data.pkl'), 'wb') as tr:
-        pickle.dump(train_dataset, tr)
-    with open(os.path.join(datasets_path, 'valid_data.pkl'), 'wb') as va:
-        pickle.dump(valid_dataset, va)
-    with open(os.path.join(datasets_path, 'test_data.pkl'), 'wb') as te:
+    test_data_path = os.path.join(args.exp_dir, 'datasets', 'test_data.pkl')
+    with open(test_data_path, 'wb') as te:
         pickle.dump(test_dataset, te)
 
     # Save y_test
     with open(os.path.join(args.exp_dir, 'y_test.pkl'), 'wb') as yt:
         pickle.dump(y_test, yt)
 
-    print('Done')
+    print('Cross-validation data preparation completed.')
 
 if __name__ == '__main__':
     import argparse
--- 2_train_model.py
+++ 2_train_model.py
@@ -1,6 +1,5 @@
 import yaml
 import os
-import random
 import torch
 import pickle
 import torch.nn as nn
@@ -9,106 +8,126 @@ from torch.utils.data import DataLoader
 from utils.utils import ModelLogger
 from utils.models import FullyConnected, FullyConnected2, LSTMModel  # Import all models you might use
 
-def train_model(train_dataset, valid_dataset, args):
-    # Use GPU if available 
+def train_model(args):
+    """
+    Trains the model using cross-validation.
+    """
+    # Use GPU if available
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print(f'Train model using {device}')
 
     # Get configurations
     config = yaml.load(open(args.conf_dir, 'r'), Loader=yaml.FullLoader)
     n_classes = config['Data_setup']['n_classes']
-    
+
     # Training setup
-    input_size = train_dataset.get_input_size()  # Automatically get input size based on dataset
     epochs = config['Training_setup']['epochs']
     batch_size = config['Training_setup']['batch_size']
     learning_rate = config['Training_setup']['learning_rate']
-    
+
     # Model setup
     dropout = config['Model_setup']['dropout']
     model_name = config['Model_setup']['model_name']  # Get the model name from config
-    
+
     # Dynamically select the model based on the config file
     model_class = globals()[model_name]  # Get the class reference from the model name
-    model = model_class(input_size=input_size, n_classes=n_classes, dropout=dropout).to(device)
-    
-    # Get the datasets and model    
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-    valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size)
-    
-    # Loss function, optimizer, and learning rate scheduler
-    criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(len(train_loader)) * 25, gamma=0.7)
-    
-    # Initialize ModelLogger
-    logger = ModelLogger(args.exp_dir)
-    
-    # Start training
-    for epoch in range(epochs):
-        train_correct = 0
-        train_total = 0
-        train_loss = []
-        model.train()
-        for batch_idx, (txt_feats, labels) in enumerate(train_loader):
-            labels = labels.to(device)
-            txt_feats = txt_feats.to(device)
-            #print(txt_feats.size())
-            outputs = model(txt_feats)
-            loss = criterion(outputs, labels)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            scheduler.step()
-            
-            train_loss.append(loss.detach().cpu().numpy())
 
-            _, predicted = torch.max(outputs.data, 1)
-            train_total += labels.size(0)
-            train_correct += (predicted == labels).sum().item()
+    # Initialize logger
+    logger = ModelLogger(args.exp_dir)
+
+    # Load cross-validation folds
+    num_folds = config['cross_validation_setup']['k_folds']
+    
+    # Iterate over each fold for cross-validation
+    for fold_idx in range(1, num_folds + 1):
+        print(f'Training fold {fold_idx}/{num_folds}...')
         
-        avg_train_loss = sum(train_loss) / len(train_loss)
-        train_acc = 100 * train_correct / train_total
+        # Load fold-specific training and validation datasets
+        dataset_path = os.path.join(args.exp_dir, 'datasets', f'fold_{fold_idx}')
+        with open(os.path.join(dataset_path, 'train_data.pkl'), 'rb') as tr:
+            train_dataset = pickle.load(tr)
+        with open(os.path.join(dataset_path, 'valid_data.pkl'), 'rb') as va:
+            valid_dataset = pickle.load(va)
         
-        # Validate on the validation set
-        model.eval()
-        with torch.no_grad():
-            val_correct = 0
-            val_total = 0
-            val_loss = []
-            for val_batch_idx, (val_txt_feats, val_labels) in enumerate(valid_loader):
-                val_labels = val_labels.to(device)
-                val_txt_feats = val_txt_feats.to(device)
-                
-                outputs = model(val_txt_feats)
-                loss = criterion(outputs, val_labels)
-                
-                val_loss.append(loss.detach().cpu().numpy())
-                
+        # Get input size from dataset
+        input_size = train_dataset.get_input_size()
+
+        # Initialize the model for this fold
+        model = model_class(input_size=input_size, n_classes=n_classes, dropout=dropout).to(device)
+
+        # DataLoader for training and validation
+        train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+        valid_loader = DataLoader(dataset=valid_dataset, batch_size=batch_size)
+
+        # Loss function, optimizer, and learning rate scheduler
+        criterion = nn.CrossEntropyLoss().to(device)
+        optimizer = optim.AdamW(model.parameters(), lr=learning_rate)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=int(len(train_loader)) * 25, gamma=0.7)
+
+        # Training loop for the current fold
+        for epoch in range(epochs):
+            model.train()
+            train_loss = []
+            train_correct, train_total = 0, 0
+            
+            for batch_idx, (txt_feats, labels) in enumerate(train_loader):
+                labels = labels.to(device)
+                txt_feats = txt_feats.to(device)
+
+                optimizer.zero_grad()
+                outputs = model(txt_feats)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                scheduler.step()
+
+                train_loss.append(loss.detach().cpu().numpy())
                 _, predicted = torch.max(outputs.data, 1)
-                val_total += val_labels.size(0)
-                val_correct += (predicted == val_labels).sum().item()
-                
-        avg_val_loss = sum(val_loss) / len(val_loss)
-        val_acc = 100 * val_correct / val_total
-        
-        lr = scheduler.get_last_lr()[0]
-        
-        # Update training history
-        logger.update(epoch, avg_train_loss, avg_val_loss, 
-                      train_acc=train_acc, val_acc=val_acc, 
-                      learning_rate=lr)
-        
-        # Save the model if the best validation loss is encountered
-        if logger.save_best_model:
-            logger.save_model(model)
-    
-        print(f"Epoch: {epoch + 1} \t train_acc: {train_acc:.2f}% \t val_acc: {val_acc:.2f}% \t train_loss: {avg_train_loss:.3f} \t val_loss: {avg_val_loss:.3f} \t LR: {lr:.2E}")
-    
+                train_total += labels.size(0)
+                train_correct += (predicted == labels).sum().item()
+
+            avg_train_loss = sum(train_loss) / len(train_loss)
+            train_acc = 100 * train_correct / train_total
+
+            # Validation loop
+            model.eval()
+            val_loss = []
+            val_correct, val_total = 0, 0
+
+            with torch.no_grad():
+                for val_batch_idx, (val_txt_feats, val_labels) in enumerate(valid_loader):
+                    val_labels = val_labels.to(device)
+                    val_txt_feats = val_txt_feats.to(device)
+
+                    outputs = model(val_txt_feats)
+                    loss = criterion(outputs, val_labels)
+                    val_loss.append(loss.detach().cpu().numpy())
+
+                    _, predicted = torch.max(outputs.data, 1)
+                    val_total += val_labels.size(0)
+                    val_correct += (predicted == val_labels).sum().item()
+
+            avg_val_loss = sum(val_loss) / len(val_loss)
+            val_acc = 100 * val_correct / val_total
+
+            lr = scheduler.get_last_lr()[0]
+
+            # Log performance for this epoch
+            logger.update(epoch, avg_train_loss, avg_val_loss,
+                          train_acc=train_acc, val_acc=val_acc,
+                          learning_rate=lr)
+
+            # Save the model if the best validation loss is encountered
+            if logger.save_best_model:
+                logger.save_model(model)
+
+            print(f"Fold {fold_idx} - Epoch {epoch + 1}: train_acc: {train_acc:.2f}%, val_acc: {val_acc:.2f}%, train_loss: {avg_train_loss:.3f}, val_loss: {avg_val_loss:.3f}, LR: {lr:.2E}")
+
+        print(f'Fold {fold_idx} completed.\n')
+
     logger.save_history()
     logger.print_best_model_info()
-    print('Training completed')
+    print('Cross-validation training completed.')
 
 if __name__ == '__main__':
     import argparse
@@ -116,12 +135,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--conf_dir', default='conf/conf.yaml')
     parser.add_argument('--exp_dir', default='exp')
-    args = parser.parse_args()
-    
-    dataset_path = os.path.join(args.exp_dir, 'datasets')    
-    tr = open(os.path.join(dataset_path, 'train_data.pkl'), 'rb') 
-    va = open(os.path.join(dataset_path, 'valid_data.pkl'), 'rb')   
-    train_dataset, valid_dataset = pickle.load(tr), pickle.load(va)
-    
-    train_model(train_dataset, valid_dataset, args)
 
+    args = parser.parse_args()
+    train_model(args)
